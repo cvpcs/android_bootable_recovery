15bd2b76024d8ada98155ceae435bd5af231c00a
cvpcs <root@cvpcs.org>
2011-01-11 13:17:11 -0600

    rofl forgot to add a file

---
+++ extracommands/extracommands.h
@@ -0,0 +1,7 @@
+#ifndef RECOVERY_EXTRACOMMANDS_H_
+#define RECOVERY_EXTRACOMMANDS_H_
+
+int flash_image_main(int argc, char* argv[]);
+int format_main(int argc, char* argv[]);
+
+#endif//RECOVERY_EXTRACOMMANDS_H_
